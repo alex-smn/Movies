36037ebf1134381947bbcb91df1e44bcf916467d
Alex <alexandr.i.semenov@gmail.com>
2024-03-14 16:37:39 +0200

    - created the project - added TCA dependency

---
+++ Movies/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Movies
+//
+//  Created by Alexander Livshits on 14/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
